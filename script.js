c25329dfca6caebfa94eaca57c416e5e11bc7180
allison <allison.villa@outlook.com>
2022-03-03 01:12:52 -0500

    added scroll effect on nav bar

---
+++ script.js
@@ -0,0 +1,11 @@
+const portfolio = {}
+
+const navEl = document.querySelector('.main-nav');
+
+window.onscroll = () => {
+    if (window.pageYOffset > 77) {
+        navEl.classList.add('scrolled');
+    } else {
+        navEl.classList.remove('scrolled');
+    }
+}

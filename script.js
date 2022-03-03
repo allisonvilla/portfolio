f372390275e8137f9c139bca5ece43266733324b
allison <allison.villa@outlook.com>
2022-03-03 01:40:24 -0500

    added comments to nav scroll logic

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ portfolio.init = () => {
     portfolio.navScroll();
 }
 
+// Apply CSS class to nav bar after specified amount of scrolling
 portfolio.navScroll = () => {
     const navEl = document.querySelector('.main-nav');
     window.onscroll = () => {
